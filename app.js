5ab98e0904614d74ccb2949d1d32dfd3b095ff4f
arifeensifat01 <arifeen.sifat01@gmail.com>
2023-09-11 18:13:59 +0600

    default error handler done

--- app.js
+++ app.js
@@ -1,31 +1,62 @@
+//external import
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 
+//internal import
+ const {notFoundHandler, errorHandler} = require("./middlewares/common/errorhandler");
+
+
 const app = express();
 dotenv.config();
 
-
-//database connection
+ //mongoose connet
 mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
-    useNewUrlParser:true,
-    useUnifiedTopology: true,
-})
+       useUnifiedTopology: true,
+       useNewUrlParser: true,
+    })
+    .then(() => console.log("database connection successful!"))
+    .catch((err) => console.log(err));
 
-.then (()=> console.log (" Database connection successful!"))
-.catch(err => console.log(err));
+
+// mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
+
+//     useUnifiedTopology: true,
+
+//     useNewUrlParser: true,
+
+// })
+// .then(console.log('connect sucess to mongodb'))
+
+   
 
 //request parsers
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
 //set view engine
-app.use("view engine", "ejs");
+app.set("view engine", "ejs");
 
 //set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
 //parse cookies
 app.use(cookieParser(process.env.COOKIE_SECRET));
+
+//routing setup
+
+
+
+// 404 not found handler
+app.use(notFoundHandler);
+
+// common error handler
+app.use(errorHandler);
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`app listning to port ${process.env.PORT}`);
+});
